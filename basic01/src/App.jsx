629a564c22449bd711c035968b7e40847f0cfc8d
romi615 <romimawandia615@gmail.com>
2025-07-12 14:07:55 +0530

    conditional rendering

--- basic01/src/App.jsx
+++ basic01/src/App.jsx
@@ -180,7 +180,6 @@
 //   );
 // }
 
-
 // export default function Profile() {
 //   return (
 //     <img
@@ -189,7 +188,6 @@
 //     />
 //   )
 // }
-
 
 // function Profile() {
 //   return (
@@ -221,3 +219,38 @@
 // }
 
 // export default App
+
+// conditional rendering
+
+function Item({ name, isPacked }) {
+  //   if (isPacked) {
+  //     // return <li className="item">{name} ✅</li>;
+  //     return null;
+  //   }
+  //   return <li className="item">{name}</li>;
+  //   return <li className="item">{isPacked ? name + " ✅ " : name} </li>;
+  // return (
+  //   <li className="item">
+  //     {name} {isPacked && " ✅ "}
+  //   </li>
+  // );
+
+  let itemContent = name;
+  if (isPacked) {
+    itemContent = name + " ✅ ";
+  }
+  return <li className="item">{itemContent}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
--- basic01/src/main.jsx
+++ basic01/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Gallery from "./App";
+// import Gallery from "./App";
 // import TodoList from "./App";
 // import Profile from "./App";
 // import List from "./App";
